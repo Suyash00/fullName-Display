06fec7e0ddd2b878f93610c72c23a23dd6f21533
Suyash00 <suyashhandy@gmail.com>
2023-12-28 13:11:00 +0530

    full name display deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (firstName.trim() !== "" && lastName.trim() !== "") {
+      setError("");
+      setIsSubmitted(true);
+    } else {
+      setError("Please enter both Firstname and Lastname");
+      setIsSubmitted(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+      {isSubmitted ? (
+        <div>
+          <form onSubmit={handleSubmit}>
+            {error && <p className="error">{error}</p>}
+            <div>
+              <label htmlFor="firstname">First Name:</label>
+              <input
+                type="text"
+                id="firstname"
+                placeholder="firstname"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="lastname">Last Name:</label>
+              <input
+                type="text"
+                id="lastname"
+                placeholder="lastname"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+
+          <p>Full Name: {`${firstName} ${lastName}`}</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+          <div>
+            <label htmlFor="firstname">First Name:</label>
+            <input
+              type="text"
+              id="firstname"
+              placeholder="firstname"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="lastname">Last Name:</label>
+            <input
+              type="text"
+              id="lastname"
+              placeholder="lastname"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
